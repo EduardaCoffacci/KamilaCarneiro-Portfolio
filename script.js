a46616c33edbf857f5156907e08b6e68f76415ee
Maria Eduarda <dudinha.coffacci@hotmail.com>
2023-04-20 17:10:01 -0300

    animacao

---
+++ script.js
@@ -0,0 +1,20 @@
+const target = document.querySelectorAll("[data-anime]");
+const animationClass = "animate";
+
+function animeScroll() {
+  const windowTop = window.pageYOffset + 600;
+  target.forEach(function (elemento) {
+    if (windowTop > elemento.offsetTop) {
+      elemento.classList.add(animationClass);
+    } else {
+      elemento.classList.remove(animationClass);
+    }
+  });
+}
+
+animeScroll();
+if (target.length) {
+  window.addEventListener("scroll", function () {
+    animeScroll();
+  });
+}
